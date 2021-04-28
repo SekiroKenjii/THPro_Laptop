8bcc593271a10689a351ab08b2a42ec50f2aba9c
Võ Trung Thường <hothy91@gmail.com>
2021-04-29 00:30:11 +0700

    Add product details Dto, add app service extension.

---
+++ Models/DTOs/TrademarkDto.cs
@@ -0,0 +1,26 @@
+﻿using Data.Enums;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Model.DTOs
+{
+    public class CreateTrademarkDto
+    {
+        [Required]
+        [StringLength(maximumLength: 20, ErrorMessage = "Trademark name is too long")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(maximumLength: 256, ErrorMessage = "Trademark name is too long")]
+        public string Description { get; set; }
+        public Status Status { get; set; }
+
+        public IFormFile Image { get; set; }
+    }
+    public class UpdateTrademarkDto : CreateTrademarkDto { }
+    public class TrademarkDto : CreateTrademarkDto
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string PublicId { get; set; }
+    }
+}
---
+++ Utility/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Data.EF;
+using Data.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Utility.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            //Identity
+            services.AddIdentity<AppUser, AppRole>()
+                .AddEntityFrameworkStores<DatabaseContext>()
+                .AddDefaultTokenProviders();
+
+            //JWT Bearer
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = config["Tokens:Issuer"],
+                    // ValidAudience = config["Tokens:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Tokens:Key"]))
+                };
+            });
+
+            return services;
+        }
+    }
+}
