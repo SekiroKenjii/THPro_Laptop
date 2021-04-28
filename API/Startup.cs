f52a5652091adea35407a81dcc50a9b99786c556
Võ Trung Thường <hothy91@gmail.com>
2021-04-29 00:01:04 +0700

    Add project files.

---
+++ API/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(_config["Swagger:OpenApiVersion"], new OpenApiInfo
+                {
+                    Title = _config["Swagger:OpenApiTitle"],
+                    Version = _config["Swagger:OpenApiVersion"]
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint(_config["Swagger:SwaggerUrl"], _config["Swagger:OpenApiTitle"]));
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors(options => options.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin());
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# ASP.NET Core 5.0 project
+## Technologies
+- ASP.NET Core 5.0
+- Entity Framework Core 5.0
+## Required Packages
+- Serilog.AspNetCore
+- Microsoft.EntityFrameworkCore.SqlServer
+- Microsoft.EntityFrameworkCore.Tools
+- Microsoft.EntityFrameworkCore.Design
+- Microsoft.Extensions.Configuration
+- Microsoft.Extensions.Configuration.FileExtensions
+- Microsoft.Extensions.Configuration.Json
+- Microsoft.AspNetCore.Identity.EntityFrameworkCore
+- AutoMapper.Extensions.Microsoft.DependencyInjection
+- CloudinaryDotNet
+- Microsoft.AspNetCore.Authentication.JwtBearer
